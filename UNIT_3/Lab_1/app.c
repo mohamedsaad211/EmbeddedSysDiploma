3021ca58ad12053de8888f49966a18c42dac0302
mohamedsaad211 <63556183+mohamedsaad211@users.noreply.github.com>
2023-10-07 22:24:05 +0300

    Delete UNIT_3/Lab_1 directory

--- UNIT_3/Lab_1/app.c
+++
@@ -1,9 +0,0 @@
-#include <uart.h>
-unsigned char string_buffer[100]="learn-in-depth:Saad";
-unsigned char const cstring_buffer[100]="learn-in-depth:Saad";
-
-void main(void)
-{
-	 uart_send_string(string_buffer);
-
-}
--- UNIT_3/Lab_1/uart.h
+++
@@ -1,5 +0,0 @@
-
-#ifndef _UART_H_
-#define _UART_H_
-void uart_send_string(unsigned char* p_tx_string);
-#endif
